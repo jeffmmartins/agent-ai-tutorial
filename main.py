c347b93d564510263d127fb4951aa6d85f23b1f1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-20 21:19:52 -0300

    Feat: finalizando projeto

--- main.py
+++ main.py
@@ -1,2 +1,54 @@
-
+from dotenv import load_dotenv
 from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from groq import Groq
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent,AgentExecutor
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+
+
+llm = ChatGroq(model ="llama3-8b-8192")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a reseach assistente tha will help generate a research paper.
+            Answer the user query and use necessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+            ),
+            ("placeholder","{chat_history}"),
+            ("human", "{query}"),
+            ("placeholder", "{agent_scratchpad}")
+            
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is capital of France"})
+print(raw_response)
+
+structured_response = parser.parse(raw_response.get("output")[0]["text"])
+print(structured_response)
+
